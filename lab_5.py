06a50833b04603d1f3074ab9fd94dad8a259ce3e
TeamSeshDeadBoy <57834332+TeamSeshDeadBoy@users.noreply.github.com>
2021-09-25 16:42:58 +0300

    Add files via upload

---
+++ lab_5.py
@@ -0,0 +1,53 @@
+from math import sqrt 
+
+
+#number 1
+print("Введите координаты двух точек для рассчета расстояния между ними в формате (x, y)") 
+x1, y1 = int(input()), int(input()) 
+x2, y2 = int(input()), int(input()) 
+ras = sqrt((x2 - x1)**2 + (y2 - y1) ** 2) 
+print("Расстояние от точки до точки = ", ras)
+
+
+#number 2
+print("Введите три точки a, b, c для рассчета расстояний между ними :")
+a, b, c = int(input()), int(input()), int(input())
+rasac = abs(c - a)
+rasbc = abs(c - b)
+print("Расстояние от точки до точки A до C = ", rasac) 
+print("Расстояние от точки до точки B до C = ", rasbc) 
+print("Сумма расстояний = ", rasac + rasbc)
+
+
+#number 3
+print("Введите три точки a, b, c для рассчета расстояний между ними :")
+a, b, c = int(input()), int(input()), int(input())
+rasac = abs(c - a)
+rasbc = abs(c - b)
+print("Расстояние от точки до точки A до C = ", rasac) 
+print("Расстояние от точки до точки B до C = ", rasbc) 
+print("Произведение расстояний = ", rasac * rasbc)
+
+
+#number 4
+print("Введите координаты противоположных вершин прямоугольника (x, y):")
+x1, y1 = int(input()), int(input())
+x2, y2 = int(input()), int(input())
+dlin = abs(x2 - x1)
+shir = abs(y2 - y1)
+print("Периметр прямоугольника P = ", 2 * (dlin + shir)) 
+print("Площадь прямоугольника S = ", dlin * shir) 
+
+
+#number 5
+print("Введите координаты трех вершин треугольника (x, y):")
+x1, y1 = int(input()), int(input())
+x2, y2 = int(input()), int(input())
+x3, y3 = int(input()), int(input())
+S = 0.5 * abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1))
+print("Площадь треугольника S = ", S) 
+
+
+
+
+
