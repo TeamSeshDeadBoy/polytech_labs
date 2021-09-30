fdefea48daab5354bacbf82c9786f6915dfbd6a0
TeamSeshDeadBoy <57834332+TeamSeshDeadBoy@users.noreply.github.com>
2021-09-30 11:54:22 +0300

    Update lab_6.py

--- lab_6.py
+++ lab_6.py
@@ -2,6 +2,7 @@
 print("Введите А, B")
 a, b = int(input()), int(input())
 b, a = a, b
+print("Переменные A, B поменяли свои значения: a - b, b - a:")
 print(a, b)
 
 
@@ -9,6 +10,7 @@ print(a, b)
 print("Введите А, B, C")
 a, b, c = int(input()), int(input()), int(input())
 b, c, a = a, b, c
+print("Переменные A, B, C поменяли свои значения: a - b, b - c, c - a:")
 print(a, b, c)
 
 
@@ -16,6 +18,7 @@ print(a, b, c)
 print("Введите А, B, C")
 a, b, c = int(input()), int(input()), int(input())
 c, b, a = a, c, b
+print("Переменные A, B, C поменяли свои значения: a - c, c - b, b - a:")
 print(a, b, c)
 
 
@@ -36,9 +39,10 @@ print("В функции y = 4(x−3)^6 − 7(x−3)^3 + 2 при данном X
 #number 6
 print("Введите переменную А = ")
 A = int(input())
-b = A**2
+b = A * A
 b = b * b
 b = b * b
+print("A^8, используя 1 вспомогательную переменную и три умножения = ")
 print(b)
 
 
@@ -50,4 +54,5 @@ c = A**3
 b = b * b * b 
 c = c * c * c 
 A = c * b
+print("A^15, используя 2 вспомогательные переменные и пять умножеений = ")
 print(A)
